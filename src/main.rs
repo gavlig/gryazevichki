27ce3b9604137337ea752673958a8e64812b9e90
gavlig <gavlig@gmail.com>
2022-04-18 00:46:56 +0200

    - spawn 4 "wheels" - dynamically link with bevy for fast building - keep some optimizations for dev config - use local version of bevy_rapier3d

--- src/main.rs
+++ src/main.rs
@@ -43,9 +43,10 @@ fn setup_graphics(mut commands: Commands) {
 		},
 		..Default::default()
 	});
+
 	commands.spawn_bundle(PerspectiveCameraBundle {
 		transform: Transform::from_matrix(Mat4::face_toward(
-			Vec3::new(-30.0, 30.0, 100.0),
+			Vec3::new(-30.0, 30.0, 30.0),
 			Vec3::new(0.0, 10.0, 0.0),
 			Vec3::new(0.0, 1.0, 0.0),
 		)),
@@ -60,59 +61,105 @@ pub fn setup_physics(mut commands: Commands) {
 	let ground_size = 200.1;
 	let ground_height = 0.1;
 
-	let collider = ColliderBundle {
+	let ground = ColliderBundle {
 		shape: ColliderShape::cuboid(ground_size, ground_height, ground_size).into(),
 		position: Vec3::new(0.0, -ground_height, 0.0).into(),
 		..ColliderBundle::default()
 	};
 
 	commands
-		.spawn_bundle(collider)
+		.spawn_bundle(ground)
 		.insert(ColliderDebugRender::default())
 		.insert(ColliderPositionSync::Discrete);
 
+	if true {
+		spawn_cubes(&mut commands);
+	}
+
+	// wheels
+	let half_height: f32 = 1.13 / 2.0;
+	let radius: f32 = 0.84;
+
+	let x = -1.414;
+	let y = 0.85;//half_height + 10.0;
+	let z = 1.468;
+	let mut pos = Vec3::new(x, y, z);
+	spawn_wheel(&pos, half_height, radius, &mut commands);
+
+	pos.x = 1.141;	
+	spawn_wheel(&pos, half_height, radius, &mut commands);
+
+	pos.x = 1.35;
+	pos.z = -1.89;
+	spawn_wheel(&pos, half_height, radius, &mut commands);
+
+	pos.x = -1.35;
+	pos.z = -1.89;
+	spawn_wheel(&pos, half_height, radius, &mut commands);
+}
+
+fn spawn_wheel(pos_in: &Vec3, half_height: f32, radius: f32, commands: &mut Commands) {
+	let rigid_body = RigidBodyBundle {
+		position: Vec3::new(pos_in.x, pos_in.y, pos_in.z).into(),
+		..RigidBodyBundle::default()
+	};
+
+	let wheel_collider = ColliderBundle {
+		shape: ColliderShape::cylinder(half_height, radius).into(),
+		..ColliderBundle::default()
+	};
+
+	commands
+		.spawn()
+		.insert_bundle(rigid_body)
+		.insert_bundle(wheel_collider)
+		.insert(ColliderDebugRender::default())
+		.insert(ColliderPositionSync::Discrete);
+}
+
+fn spawn_cubes(commands: &mut Commands) {
 	/*
 	 * Create the cubes
 	 */
-	let num = 8;
-	let rad = 1.0;
-
-	let shift = rad * 2.0 + rad;
-	let centerx = shift * (num / 2) as f32;
-	let centery = shift / 2.0;
-	let centerz = shift * (num / 2) as f32;
-
-	let mut offset = -(num as f32) * (rad * 2.0 + rad) * 0.5;
-	let mut color = 0;
-
-	for j in 0usize..20 {
-		for i in 0..num {
-			for k in 0usize..num {
-				let x = i as f32 * shift - centerx + offset;
-				let y = j as f32 * shift + centery + 3.0;
-				let z = k as f32 * shift - centerz + offset;
-				color += 1;
-
-				// Build the rigid body.
-				let rigid_body = RigidBodyBundle {
-					position: Vec3::new(x, y, z).into(),
-					..RigidBodyBundle::default()
-				};
-
-				let collider = ColliderBundle {
-					shape: ColliderShape::cuboid(rad, rad, rad).into(),
-					..ColliderBundle::default()
-				};
-
-				commands
-					.spawn()
-					.insert_bundle(rigid_body)
-					.insert_bundle(collider)
-					.insert(ColliderDebugRender::with_id(color))
-					.insert(ColliderPositionSync::Discrete);
-			}
-		}
-
-		offset -= 0.05 * rad * (num as f32 - 1.0);
-	}
+	 let num = 8;
+	 let rad = 1.0;
+ 
+	 let shift = rad * 2.0 + rad;
+	 let centerx = shift * (num / 2) as f32;
+	 let centery = shift / 2.0;
+	 let centerz = shift * (num / 2) as f32;
+ 
+	 let mut offset = -(num as f32) * (rad * 2.0 + rad) * 0.5;
+	 let mut color = 0;
+ 
+	 for j in 0usize..20 {
+		 for i in 0..num {
+			 for k in 0usize..num {
+				 let x = i as f32 * shift - centerx + offset;
+				 let y = j as f32 * shift + centery + 3.0;
+				 let z = k as f32 * shift - centerz + offset;
+				 color += 1;
+ 
+				 // Build the rigid body.
+				 let rigid_body = RigidBodyBundle {
+					 position: Vec3::new(x, y, z).into(),
+					 ..RigidBodyBundle::default()
+				 };
+ 
+				 let collider = ColliderBundle {
+					 shape: ColliderShape::cuboid(rad, rad, rad).into(),
+					 ..ColliderBundle::default()
+				 };
+ 
+				 commands
+					 .spawn()
+					 .insert_bundle(rigid_body)
+					 .insert_bundle(collider)
+					 .insert(ColliderDebugRender::with_id(color))
+					 .insert(ColliderPositionSync::Discrete);
+			 }
+		 }
+ 
+		 offset -= 0.05 * rad * (num as f32 - 1.0);
+	 }
 }
