fb4f22b153a6f03d516a6d065cfcca1900bf223e
gavlig <gavlig@gmail.com>
2022-04-23 23:40:02 +0200

    milestone: two rotating wheels with correct orientation

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use bevy_rapier3d::prelude::*;
 use bevy_fly_camera::{FlyCamera, FlyCameraPlugin};
 //use bevy_editor_pls::prelude::*;
 use bevy::app::AppExit;
+use nalgebra as nalg;
 
 use bevy::render::mesh::shape as render_shape;
 
@@ -114,98 +115,108 @@ pub fn setup_physics(mut commands: Commands) {
 	}
 
 	// wheels 1.13
-	let mut half_height: f32 = 1.13 / 2.0;
-	let mut radius: f32 = 0.84;
+	let mut half_height: f32 = 0.5;
+	let mut radius: f32 = 0.80;
 
-	let x = -1.414;
-	let y = 0.85; // 0.85
-	let z = 1.468;
-	let mut pos = Vec3::new(x, y, z);
-//	let rf_wheel = spawn_wheel(&pos, half_height, radius, &mut commands);
-
-	pos.x = 1.141;	
+//	pos.x = 1.141;	
 //	let lf_wheel = spawn_wheel(&pos, half_height, radius, &mut commands);
 
-	pos.x = 1.35;
-	pos.z = -1.89;
+//	pos.x = 1.35;
+//	pos.z = -1.89;
 //	let lr_wheel = spawn_wheel(&pos, half_height, radius, &mut commands);
 
-	pos.x = -1.35;
-	pos.z = -1.89;
+//	pos.x = -1.35;
+//	pos.z = -1.89;
 //	let rr_wheel = spawn_wheel(&pos, half_height, radius, &mut commands);
 
-	pos.x = 0.0;
-	pos.y = 1.791;
-	pos.z = -0.271;
-	let half_size = Vec3::new(0.55, 0.939, 2.599); // original x half size is 0.791
-//	let body = spawn_box(&pos, &half_size, &mut commands);
+	let pos = Vec3::new(0.0, 3.5, 0.0);
+	let half_size = Vec3::new(0.5, 0.5, 1.0);
+	let body = spawn_box(&pos, &half_size, RigidBodyType::Static, &mut commands);
 
-	let locked_axes = JointAxesMask::ANG_X;
-	let limited_axes = JointAxesMask::X
-			| JointAxesMask::Y
-			| JointAxesMask::Z
-			| JointAxesMask::ANG_Y
-			| JointAxesMask::ANG_Z;
+	{
+		let rf_wheel = spawn_wheel(half_height, radius, RigidBodyType::Dynamic, &mut commands);
 
-	let mut anchor = point![-0.864, 0.85, 1.468];
+		let anchor1 = point![1.6,-0.8,-1.4];
+		let anchor2 = point![0.0,0.0,0.0];
+		//let anchor2 = point![-1.6,0.8,1.4];
 
-//	create_6dof_joint(rf_wheel, body, locked_axes, anchor, &mut commands)
+		create_6dof_joint(body, rf_wheel, anchor1, anchor2, &mut commands)
+	}
+
+	{
+		let lf_wheel = spawn_wheel(half_height, radius, RigidBodyType::Dynamic, &mut commands);
+
+		let anchor1 = point![-1.6,-0.8,-1.4];
+		let anchor2 = point![0.0,0.0,0.0];
+		//let anchor2 = point![-1.6,0.8,1.4];
+
+		create_6dof_joint(body, lf_wheel, anchor1, anchor2, &mut commands)
+	}
 
 	//
 	//
 	//
 
-	radius = 0.4;
-	half_height = 0.4;
+	//radius = 0.4;
+	//half_height = 0.4;
 
 	//pos.x = 0.0;
 	//pos.y = 2.0;
 	//pos.z = 0.0;
-	pos.x = 0.0;
-	pos.y = 2.0;
-	pos.z = 0.0;
-	let wheel1 = spawn_wheel(&pos, half_height, radius, RigidBodyType::Static, &mut commands);
+	//pos.x = 0.0;
+	//pos.y = 2.0;
+	//pos.z = 0.0;
+	//let wheel1 = spawn_wheel(&pos, half_height, radius, RigidBodyType::Static, &mut commands);
 
 	//pos.x = 1.0;
 	//pos.y = 2.0;
 	//pos.z = 0.0;
-	pos.x = 0.0;
-	pos.y = 2.0;
-	pos.z = 1.0;
-	let wheel2 = spawn_wheel(&pos, half_height, radius, RigidBodyType::Dynamic, &mut commands);
+	//pos.x = 0.0;
+	//pos.y = 2.0;
+	//pos.z = 1.0;
+	//let wheel2 = spawn_wheel(&pos, half_height, radius, RigidBodyType::Dynamic, &mut commands);
 
-	let anchor1 = point![0.0,0.0,0.0];
-	let anchor2 = point![0.0,0.0,-1.0];
+	//let anchor1 = point![0.0,0.0,0.0];
+	//let anchor2 = point![-1.0,0.0,0.0];
+	//let anchor2 = point![0.0,0.0,-1.0];
 
-	create_6dof_joint(wheel1, wheel2, locked_axes, anchor1, anchor2, &mut commands);
+	//create_6dof_joint(wheel1, wheel2, locked_axes, anchor1, anchor2, &mut commands);
 }
 
-fn create_6dof_joint(entity1: Entity, entity2: Entity, locked_axes: JointAxesMask, anchor1: nalgebra::Point3<Real>, anchor2: nalgebra::Point3<Real>, commands: &mut Commands) {//, origin: Point3<f32>, num: usize) {
-	let mut joint_6dof = RevoluteJoint::new(Vector::z_axis()).local_anchor1(anchor1).local_anchor2(anchor2); // JointAxesMask::FREE_FIXED_AXES
+fn create_6dof_joint(entity1: Entity, entity2: Entity, anchor1: nalgebra::Point3<Real>, anchor2: nalgebra::Point3<Real>, commands: &mut Commands) {
+	let mut joint_6dof = RevoluteJoint::new(Vector::x_axis()).local_anchor1(anchor1).local_anchor2(anchor2);
+	joint_6dof = joint_6dof.motor_velocity(1., 0.1);
 
-	commands.spawn_bundle((JointBuilderComponent::new(joint_6dof, entity1, entity2),));
+	commands.spawn().insert(JointBuilderComponent::new(joint_6dof, entity1, entity2));
 }
 
-fn spawn_wheel(pos_in: &Vec3, half_height: f32, radius: f32, body_type: RigidBodyType,commands: &mut Commands) -> Entity {
-	let mut component = RigidBodyPositionComponent::default();
-	component.position.translation = Vec3::new(pos_in.x, pos_in.y, pos_in.z).into();
+fn spawn_wheel(/* pos_in: &Vec3, */ half_height: f32, radius: f32, body_type: RigidBodyType, commands: &mut Commands) -> Entity {
+//	let mut component = RigidBodyPositionComponent::default();
+//	component.position.translation = pos_in.clone().into();// Vec3::new(pos_in.x, pos_in.y, pos_in.z).into();
 
 	//let axis = Unit::new_normalize(Vector3::new(1.0, 2.0, 3.0));
 	//let angle = 1.2;
 	//let rot = UnitQuaternion::from_axis_angle(&axis, angle);
 	let rot = nalgebra::UnitQuaternion::from_axis_angle(&nalgebra::Vector3::z_axis(), std::f32::consts::FRAC_PI_2);
-	//component.position.rotation = rot;
+//	component.position.rotation = rot;
 
 	let rigid_body = RigidBodyBundle {
-		position: component,
+//		position: component,
 		body_type: RigidBodyTypeComponent(body_type),
 		..RigidBodyBundle::default()
 	};
 
+	let collider_position = nalg::Isometry3 {
+		rotation: rot,
+		..Default::default()
+	};
+
 	let wheel_collider = ColliderBundle {
-//		shape: ColliderShape::cylinder(half_height, radius).into(),
+		shape: ColliderShape::cylinder(half_height, radius).into(),
+		position: collider_position.into(),
 //		shape: ColliderShape::ball(radius).into(),
-		shape: ColliderShape::cuboid(half_height, half_height, half_height).into(),
+//		shape: ColliderShape::cuboid(half_height, half_height, half_height).into(),
+
 		flags: (ActiveEvents::INTERSECTION_EVENTS | ActiveEvents::CONTACT_EVENTS).into(),
 		..ColliderBundle::default()
 	};
@@ -219,12 +230,13 @@ fn spawn_wheel(pos_in: &Vec3, half_height: f32, radius: f32, body_type: RigidBod
 		.id()
 }
 
-fn spawn_box(pos_in: &Vec3, half_size: &Vec3, commands: &mut Commands) -> Entity {
+fn spawn_box(pos_in: &Vec3, half_size: &Vec3, body_type: RigidBodyType, commands: &mut Commands) -> Entity {
 	let mut component = RigidBodyPositionComponent::default();
 	component.position.translation = Vec3::new(pos_in.x, pos_in.y, pos_in.z).into();
 
 	let rigid_body = RigidBodyBundle {
 		position: component,
+		body_type: RigidBodyTypeComponent(body_type),
 		..RigidBodyBundle::default()
 	};
 
